c6cf871ad9409652c0e554309a59dc089dce4b35
Leul Mesfin <leulmesfin152025@gmail.com>
2025-04-16 02:01:51 -0400

    used fastapi to create a post endpoint so the client can send a user's address to my backend code. Used pydantic to validate the request body schema, then used the requests library to make an api call to the geocoding api to grab the geolocation of the client based on their address, I then send the geocode back to the client

--- shoppersScraper.py
+++ shoppersScraper.py
@@ -1,100 +1,143 @@
 from playwright.sync_api import sync_playwright, Playwright
-import re, json, os
+import re, json,  os
 from dotenv import load_dotenv
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+import requests
 
 load_dotenv()
+app = FastAPI()
 user_data_dir = "./user-data"
 
-with sync_playwright() as p:
-    # basic playwright setup
-    chrome = p.chromium
-    browser = chrome.launch(headless=False)
-    context = browser.new_context()
-    page = context.new_page()
+origins = [
+    "http://localhost",
+    "http://localhost:5173",
+]
 
-    # Set geolocation
-    context.set_geolocation({ "latitude": os.getenv("LATITUDE"), "longitude": os.getenv("LONGITUDE") })
-    # Grant geolocation
-    context.grant_permissions(["geolocation"])
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class Item(BaseModel):
+    city: str
+    state: str
+    zip: int
+
+@app.post("/grabGeolocation")
+async def grabGeolocation(item: Item):
+    city = item.city
+    state = item.state
+    zip = item.zip
+
+    try:
+        resp = requests.get(f"https://geocode.maps.co/search?q={city}%2C+{state}+{zip}&api_key={os.getenv("GEOCODE_API_KEY")}")
+        # latitude = resp[0]["lat"]
+        # longitude = resp[0]["lon"]
+        json_data = resp.json()
+        loc = json_data[0]
+        latitude = loc["lat"]
+        longitude = loc["lon"]
+        return {"Geocode": f"lat: {latitude}, lon: {longitude}"}
+    except requests.exceptions.RequestException as e:
+        raise SystemExit(e)
+    
+    # return {"Location": f"{item.city}, {item.state} {item.zip}"}
+
+# with sync_playwright() as p:
+#     # basic playwright setup
+#     chrome = p.chromium
+#     browser = chrome.launch(headless=False)
+#     context = browser.new_context()
+#     page = context.new_page()
+
+#     # Set geolocation
+#     context.set_geolocation({ "latitude": 39.083997, "longitude": -77.161263})
+#     # Grant geolocation
+#     context.grant_permissions(["geolocation"])
 
 
-    url = "https://www.shoppersfood.com/search/products?q=eggs"
-    page.goto(url)
+#     url = "https://www.shoppersfood.com/search/products?q=eggs"
+#     page.goto(url)
 
-    selector = "button[data-testid='store-select-link']"
-    page.locator(selector).click()
+#     selector = "button[data-testid='store-select-link']"
+#     page.locator(selector).click()
 
-    selector = "button[aria-describedBy='shop2362']"
-    page.locator(selector).click()
+#     selector = "button[aria-describedBy='shop2362']"
+#     page.locator(selector).click()
    
 
-    selector = "div.product-grid"
-    productDiv = page.locator(selector)
-    children = productDiv.locator("div.product-card")
-    print("products: ", children.count())
-    eggProducts = []
-    price_float = 0.0
-    shoppers_dict = {}
-    shoppers_dict['ShoppersEggs'] = []
+#     selector = "div.product-grid"
+#     productDiv = page.locator(selector)
+#     children = productDiv.locator("div.product-card")
+#     print("products: ", children.count())
+#     eggProducts = []
+#     price_float = 0.0
+#     shoppers_dict = {}
+#     shoppers_dict['ShoppersEggs'] = []
 
-    # This function takes in a product: string and a threshold: int
-    # The purpose of this function is to assign a score to the input string
-    # If the score is >= threshold, there is high likelihood that the string is an egg carton, else there is
-    # a high likelihood that the string is not an egg carton. 
-    # This function returns True if the score is >= threshold, else it returns false
-    def egg_carton_score(product, threshold):
-        # strong indicators: +3 points
-        # weak indicators: +1 point
-        # strong exclusion terms: -3 points
-        # weak exclusion terms: -1 point
-        strong_indicators = ["Grade AA", "Grade A", "Grade B", "Fresh Eggs", "Free Range"]
-        # sizing shouldn't hold too much wait b/c any egg product can have sizes
-        weak_indicators = ["Eggs", "Large", "Medium", "Small", "Extra Large", "Jumbo", "12 count", "18 count", "24 count"]
-        strong_exclusionary_terms = ["Egg Whites", "Liquid Egg", "Egg Substitute", "Deviled Egg", "Egg White", "Egg Beaters", "Eggplant", "Egg Roll"]
-        weak_exclusion_terms = ["Welch's", "Jimmy Dean", "Easter", "Eggo", "Burrito", "Swaggerty's", "Brach's", "Pancakes", "Waffles", "Sausage", "Croissant", "Kellogg's"]
-        score = 0
+#     # This function takes in a product: string and a threshold: int
+#     # The purpose of this function is to assign a score to the input string
+#     # If the score is >= threshold, there is high likelihood that the string is an egg carton, else there is
+#     # a high likelihood that the string is not an egg carton. 
+#     # This function returns True if the score is >= threshold, else it returns false
+#     def egg_carton_score(product, threshold):
+#         # strong indicators: +3 points
+#         # weak indicators: +1 point
+#         # strong exclusion terms: -3 points
+#         # weak exclusion terms: -1 point
+#         strong_indicators = ["Grade AA", "Grade A", "Grade B", "Fresh Eggs", "Free Range"]
+#         # sizing shouldn't hold too much wait b/c any egg product can have sizes
+#         weak_indicators = ["Eggs", "Large", "Medium", "Small", "Extra Large", "Jumbo", "12 count", "18 count", "24 count"]
+#         strong_exclusionary_terms = ["Egg Whites", "Liquid Egg", "Egg Substitute", "Deviled Egg", "Egg White", "Egg Beaters", "Eggplant", "Egg Roll"]
+#         weak_exclusion_terms = ["Welch's", "Jimmy Dean", "Easter", "Eggo", "Burrito", "Swaggerty's", "Brach's", "Pancakes", "Waffles", "Sausage", "Croissant", "Kellogg's"]
+#         score = 0
 
-        for term in strong_indicators:
-            if term.lower() in product.lower():
-                score += 3
-        for term in weak_indicators:
-            if term.lower() in product.lower():
-                score += 1
-        for term in strong_exclusionary_terms:
-            if term.lower() in product.lower():
-                score -= 3
-        for term in weak_exclusion_terms:
-            if term.lower() in product.lower():
-                score -= 1
+#         for term in strong_indicators:
+#             if term.lower() in product.lower():
+#                 score += 3
+#         for term in weak_indicators:
+#             if term.lower() in product.lower():
+#                 score += 1
+#         for term in strong_exclusionary_terms:
+#             if term.lower() in product.lower():
+#                 score -= 3
+#         for term in weak_exclusion_terms:
+#             if term.lower() in product.lower():
+#                 score -= 1
         
-        # print(f"{product}, score: {score}")
-        if score >= threshold:
-            return True
-        return False
+#         # print(f"{product}, score: {score}")
+#         if score >= threshold:
+#             return True
+#         return False
 
-    # filter products so I only grab egg cartons + store in json
-    for i in range(children.count()):
-        child = children.nth(i)
-        name = child.locator("div.product-details-wrapper h3").text_content()
-        # assuming the price and/or sales price is the first p tag (seems to hold true for the shoppers site as of 4/13/2025)
-        price = child.locator("div.product-details-wrapper p").nth(0).text_content() 
+#     # filter products so I only grab egg cartons + store in json
+#     for i in range(children.count()):
+#         child = children.nth(i)
+#         name = child.locator("div.product-details-wrapper h3").text_content()
+#         # assuming the price and/or sales price is the first p tag (seems to hold true for the shoppers site as of 4/13/2025)
+#         price = child.locator("div.product-details-wrapper p").nth(0).text_content() 
     
-        extracted_price = (re.search("(\d{1,2}\.\d{2})", price))
-        if extracted_price:
-            price_float = float(extracted_price[0])
+#         extracted_price = (re.search("(\d{1,2}\.\d{2})", price))
+#         if extracted_price:
+#             price_float = float(extracted_price[0])
         
-        # print out the items that are likely to be an egg carton
-        if egg_carton_score(name, 3):
-            # json_prod = json.dumps({"name": name, "price": price_float}, indent=2) # python dict -> json string
-            eggProducts.append({"name": name, "price": price_float})
-            # print(f"Item {i + 1} name: {name} price: ${price_float} ")
+#         # print out the items that are likely to be an egg carton
+#         if egg_carton_score(name, 3):
+#             # json_prod = json.dumps({"name": name, "price": price_float}, indent=2) # python dict -> json string
+#             eggProducts.append({"name": name, "price": price_float})
+#             # print(f"Item {i + 1} name: {name} price: ${price_float} ")
 
-    shoppers_dict['products'] = eggProducts
-    shoppers_json = json.dumps(shoppers_dict, indent=2)
-    with open('shoppers.json', 'w') as f:
-        f.write(shoppers_json)
+#     shoppers_dict['products'] = eggProducts
+#     shoppers_json = json.dumps(shoppers_dict, indent=2)
+#     with open('shoppers.json', 'w') as f:
+#         f.write(shoppers_json)
 
             
             
-    input("Press Enter to close the browser...")
+#     input("Press Enter to close the browser...")
     
